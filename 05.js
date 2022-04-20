7a4f76ce9e1553c34f342e8d1b02e13a185dc20f
sabdahtb <sabdahutabarat2804@gmail.com>
2022-04-20 14:43:27 +0700

    cleaning 05.js

--- 05.js
+++ 05.js
@@ -1,13 +1,3 @@
-// Buatlah script untuk menentukan rating suatu film, dengan ketentuan sebagai berikut:
-
-// Jika usia yang dimasukkan lebih atau sama dengan 21, maka rating film adalah DEWASA.
-// Jika usia yang dimasukkan lebih atau sama dengan 13, maka rating film adalah REMAJA.
-// Jika usia yang dimasukkan lebih atau sama dengan 9, maka rating film adalah BIMBINGAN ORANG TUA.
-// Jika usia yang dimasukkan kurang dari 9, maka rating film adalah SEMUA USIA.
-// Contoh:
-// Input: 15
-// Output: Remaja
-
 function rating(umur) {
     if(umur >= 21)return "DEWASA";
     else if(umur >= 13) return "REMAJA";
