bbeedf9782c617151bb93501d535e1c081bee465
sabdahtb <sabdahutabarat2804@gmail.com>
2022-04-20 15:28:41 +0700

    create rating umur

--- 05.js
+++ 05.js
@@ -2,8 +2,8 @@ function rating(umur) {
     if(umur >= 21)return "DEWASA";
     else if(umur >= 13) return "REMAJA";
     else if(umur >= 9) return "BIMBINGAN ORANG TUA";
-    else if(umur < 9) return "SEMUA USIA";
+    else return "SEMUA USIA";
 }
 
 console.log(rating(15))
-console.log(rating(32))
+console.log(rating(8))
